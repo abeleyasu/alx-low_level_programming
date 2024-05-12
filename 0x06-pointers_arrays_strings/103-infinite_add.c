3e5d0feeb4da67101da1cd2f0a2c805d616e45ce
Abel Eyasu <abeleyasu98@gmail.com>
2024-05-12 04:01:28 -0700

    last task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,57 +2,50 @@
 
 /**
  * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
+ * @n1: first number to add
+ * @n2: second number to add
  * @r: buffer to store the result
  * @size_r: buffer size
- * Return: pointer to the result, or 0 if result cannot be stored in r
+ *
+ * Return: pointer to the result, or 0 if result cannot be stored
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
-	int i, j;
+	int i, j, k, l, m, n;
 
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
-
-	if (len1 > size_r || len2 > size_r)
+	for (l = 0; n1[l]; l++)
+		;
+	for (m = 0; n2[m]; m++)
+		;
+	if (l > size_r || m > size_r)
 		return (0);
-
-	len1--;
-	len2--;
-	r[size_r] = '\0';
-	size_r--;
-
-	for (i = len1, j = len2; i >= 0 || j >= 0; i--, j--)
+	i = l - 1;
+	j = m - 1;
+	k = 0;
+	n = 0;
+	while (i >= 0 || j >= 0)
 	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-		if (sum > 9)
-		{
-			carry = 1;
-			sum %= 10;
-		}
-		else
-		{
-			carry = 0;
-		}
-		r[size_r] = sum + '0';
-		size_r--;
-	}
-
-	if (carry)
-	{
-		if (size_r < 0)
+		n += (i >= 0) ? n1[i--] - '0' : 0;
+		n += (j >= 0) ? n2[j--] - '0' : 0;
+		if (k >= size_r - 1 || (n > 9 && (r[k++] = n % 10 + '0')))
 			return (0);
-		r[size_r] = '1';
-		return (r + size_r);
+		else if ((n <= 9 && (r[k++] = n + '0')))
+			n = 0;
+		else
+			n /= 10;
 	}
-
-	return (r + size_r + 1);
+	if (n)
+	{
+		if (k >= size_r - 1)
+			return (0);
+		r[k++] = n + '0';
+	}
+	r[k] = '\0';
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		n = r[i];
+		r[i] = r[j];
+		r[j] = n;
+	}
+	return (r);
 }
