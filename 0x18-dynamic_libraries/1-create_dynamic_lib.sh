b0e18ec3693f5559b95fe991b1756316093e2386
Abel Eyasu <abeleyasu98@gmail.com>
2024-05-25 11:43:53 -0700

    task 0

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+# Compile all .c files into object files
+gcc -c -Wall -Werror -Wextra -fPIC *.c
+
+# Create the dynamic library from the object files
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
