5fbbd28dd16a0d48c041473128ea38e96f4edd08
abeleyasu <abeleyasu98@gmail.com>
2024-08-01 00:47:07 -0700

    task 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,54 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * infinite_add - adds two numbers
- * @n1: first number to add
- * @n2: second number to add
- * @r: buffer to store the result
- * @size_r: buffer size
- *
- * Return: pointer to the result, or 0 if result cannot be stored
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	for (l = 0; n1[l]; l++)
-		;
-	for (m = 0; n2[m]; m++)
-		;
-	if (l > size_r || m > size_r)
-		return (0);
-	i = l - 1;
-	j = m - 1;
-	k = 0;
-	n = 0;
-	while (i >= 0 || j >= 0)
-	{
-		n += (i >= 0) ? n1[i--] - '0' : 0;
-		n += (j >= 0) ? n2[j--] - '0' : 0;
-		if (k >= size_r - 1 || (n > 9 && (r[k++] = n % 10 + '0')))
-			return (0);
-		else if ((n <= 9 && (r[k++] = n + '0')))
-			n = 0;
-		else
-			n /= 10;
-	}
-	if (n)
-	{
-		if (k >= size_r - 1)
-			return (0);
-		r[k++] = n + '0';
-	}
-	r[k] = '\0';
-	for (i = 0, j = k - 1; i < j; i++, j--)
-	{
-		n = r[i];
-		r[i] = r[j];
-		r[j] = n;
-	}
-	return (r);
+while (*(n1 + c1) != '\0')
+c1++;
+while (*(n2 + c2) != '\0')
+c2++;
+if (c1 >= c2)
+bg = c1;
+else
+bg = c2;
+if (size_r <= bg + 1)
+return (0);
+r[bg + 1] = '\0';
+c1--, c2--, size_r--;
+dr1 = *(n1 + c1)-48, dr2 = *(n2 + c2)-48;
+while (bg >= 0)
+{
+op = dr1 + dr2 + add;
+if (op >= 10)
+add = op / 10;
+else
+add = 0;
+if (op > 0)
+*(r + bg) = (op % 10) + 48;
+else
+*(r + bg) = '0';
+if (c1 > 0)
+c1--, dr1 = *(n1 + c1)-48;
+else
+dr1 = 0;
+if (c2 > 0)
+c2--, dr2 = *(n2 + c2)-48;
+else
+dr2 = 0;
+bg--, size_r--;
+}
+if (*(r) == '0')
+return (r + 1);
+else
+return (r);
 }
